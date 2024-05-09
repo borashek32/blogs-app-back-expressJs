3fa0bcbceec4fb53fdb2e3aa9f61bf1d0cef0d8f
Nataly Baranova <borashek@inbox.ru>
2024-05-09 13:49:30 +0300

    feat: gh-pages try

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import router from "./routes/app";
+import cors from "cors";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+}
+
+app.use(cors(corsOptions));
 app.use("/api", router);
 
 app.listen(PORT, () => {
